4b5a817a7c1bc4abd48e06878a227f192f87cca7
al26 <31477016+al26@users.noreply.github.com>
2018-10-10 11:35:08 +0700

    bank update otw

--- resources/assets/js/admin/main.js
+++ resources/assets/js/admin/main.js
@@ -33,7 +33,44 @@ $(document).ready(function() {
 	// 	$('.user-menu').parent().removeClass('open');
 	// 	$('.user-menu').parent().toggleClass('open');
 	// });
-
-
-
 });
+
+isNumberKey = function(evt){
+	var charCode = (evt.which) ? evt.which : event.keyCode
+	if (charCode > 31 && (charCode < 48 || charCode > 57))
+		return false;
+	return true;
+}
+
+previewImgUpload = function (input, def, loader, prev, label) {
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+
+		reader.onload = function() {
+			if($(def).is(":visible")) {
+				$(def).hide(100);
+			}
+			if($(prev).is(":visible")) {
+				$(prev).hide(100);
+			}
+		}
+
+		reader.onprogress = function(data) {
+			$(loader).show(100);
+			var timer = 2;
+			var x = setInterval(function(){
+				timer--;
+
+				if(timer <= 0) {
+					clearInterval(x);
+					$(loader).hide(100);
+					$(prev).attr('src', reader.result);
+					$(prev).show(100);
+					$(label).text(input.files[0].name);
+				}
+			}, 1000);
+		}
+
+		reader.readAsDataURL(input.files[0]);
+	}
+}
